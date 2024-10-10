a4099283f765ede8f7be2191edf798d8d3f82f7e
Abhijeet Nag <abhijeetnag@Abhijeets-MacBook-Pro.local>
2024-10-10 17:10:24 +0530

    Task Completed

--- lib/screens/image_gallery_screen.dart
+++ lib/screens/image_gallery_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_gallery/screens/image_show.dart';
 import '../models/image_model.dart';
 import '../services/pixabay_service.dart';
 import '../widgets/image_card.dart';
@@ -32,7 +33,8 @@ class _ImageGalleryScreenState extends State<ImageGalleryScreen> {
     });
 
     try {
-      List<ImageModel> fetchedImages = await PixabayService.fetchImages(_page, imageCount: imageCount);
+      List<ImageModel> fetchedImages =
+          await PixabayService.fetchImages(_page, imageCount: imageCount);
       setState(() {
         _images.addAll(fetchedImages);
         _page++;
@@ -50,7 +52,8 @@ class _ImageGalleryScreenState extends State<ImageGalleryScreen> {
   }
 
   bool _onScrollNotification(ScrollNotification scrollInfo) {
-    if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent && !_isLoading) {
+    if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent &&
+        !_isLoading) {
       _fetchImages(_calculateImagesToLoad());
     }
     return true;
@@ -59,7 +62,8 @@ class _ImageGalleryScreenState extends State<ImageGalleryScreen> {
   int _calculateImagesToLoad() {
     final double screenHeight = MediaQuery.of(context).size.height;
     final int columns = (MediaQuery.of(context).size.width / 150).floor();
-    final double rowHeight = 150; // Approximate height for an image with padding and info
+    final double rowHeight =
+        150; // Approximate height for an image with padding and info
     final int rows = (screenHeight / rowHeight).ceil();
 
     return rows * columns; // Calculate total images needed
@@ -78,20 +82,25 @@ class _ImageGalleryScreenState extends State<ImageGalleryScreen> {
         child: _images.isEmpty && !_isLoading
             ? const Center(child: CircularProgressIndicator())
             : GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: columns,
-            crossAxisSpacing: 4.0,
-            mainAxisSpacing: 4.0,
-          ),
-          itemCount: _images.length + (_isLoading ? 1 : 0),
-          itemBuilder: (context, index) {
-            if (index == _images.length) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            final image = _images[index];
-            return ImageCard(image: image);
-          },
-        ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: columns,
+                  crossAxisSpacing: 4.0,
+                  mainAxisSpacing: 4.0,
+                ),
+                itemCount: _images.length + (_isLoading ? 1 : 0),
+                itemBuilder: (context, index) {
+                  if (index == _images.length) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  final image = _images[index];
+                  return GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => ImageShow(image: image,)));
+                    },
+                    child: ImageCard(image: image),
+                  );
+                },
+              ),
       ),
     );
   }
